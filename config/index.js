6cdcdbfaae097452041a1266ac502e7c24e2b639
Vinayak <inbox.vinayak@gmail.com>
2018-02-20 00:22:37 +0530

    changed assetsPublicPath

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
-'use strict'
+'use strict';
 
 // see http://vuejs-templates.github.io/webpack for documentation.
-const path = require('path')
+const path = require('path');
 
 module.exports = {
   build: {
@@ -9,7 +9,7 @@ module.exports = {
     index: path.resolve(__dirname, '../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
@@ -21,20 +21,20 @@ module.exports = {
     // View the bundle analyzer report after build finishes:
     // `npm run build --report`
     // Set to `true` or `false` to always turn it on or off
-    bundleAnalyzerReport: process.env.npm_config_report
+    bundleAnalyzerReport: process.env.npm_config_report,
   },
   dev: {
     env: require('./dev.env'),
     port: 8080,
     autoOpenBrowser: true,
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '',
     proxyTable: {},
     // CSS Sourcemaps off by default because relative paths are "buggy"
     // with this option, according to the CSS-Loader README
     // (https://github.com/webpack/css-loader#sourcemaps)
     // In our experience, they generally work as expected,
     // just be aware of this issue when enabling this option.
-    cssSourceMap: false
-  }
-}
+    cssSourceMap: false,
+  },
+};
--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,6 @@ Vue.use(Router);
 
 export default new Router({
   mode: 'history',
-  base: process.env.NODE_ENV === 'development' ? '/' : '/quotes/',
   routes: [
     {
       path: '/',
